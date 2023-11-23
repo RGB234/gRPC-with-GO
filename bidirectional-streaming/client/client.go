80456e45a04f820ae3482e5390084682bf777fb6
rgb234 <fehs0611@gmail.com>
2023-11-24 03:01:51 +0900

    bidirectional

--- bidirectional-streaming/client/client.go
+++ bidirectional-streaming/client/client.go
@@ -1,31 +1,116 @@
+/*
+ref. https://grpc.io/docs/languages/go/basics/
+ref. https://github.com/grpc/grpc-go//examples/route_guide
+
+Note that in gRPC-Go, RPCs operate in a blocking/synchronous mode,
+which means that the RPC call waits for the server to respond
+
+Although each side will always get the other’s messages in the order they were written,
+both the client and server can read and write in any order — the streams operate completely independently.
+*/
+
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"time"
 
 	pb "github.com/rgb234/gRPC-with-GO/bidirectional-streaming/pbs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func make_message(message string) pb.Message {
-	return pb.Message{Message:message}
+var (
+	addr =  flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+
+func make_message(message string) *pb.Message {
+	return &pb.Message{Message:message}
 }
 
-func generate_messages() pb.Message {
-	var messages []pb.Message = []pb.Message {
-		make_message("message #1"),
-		make_message("message #2"),
-		make_message("message #3"),
-		make_message("message #4"),
-		make_message("message #5"),
+func generate_messages() chan *pb.Message {
+	messages := []string{
+		"message #1",
+		"message #2",
+		"message #3",
+		"message #4",
+		"message #5",
 	}
-	for _, msg := range(messages) {
-		fmt.Printf("[client to server] %v", msg.Message)
-		return msg.Message
-	}
+	// buffered channel, buffer size : len(messages)
+	messageChan := make(chan *pb.Message, len(messages))
+
+	// yield
+	// ref. http://golang.site/go/article/22-Go-%EC%B1%84%EB%84%90
+	// 익명함수 정의, 별도의 goroutine 으로 익명함수 실행
+	go func() { 
+		// 해당 익명함수 종료시 (for 구문으로 모든 메시지를 전송했을 시) 채널닫기
+		defer close(messageChan) 
+		// channel range 구문
+		for _, message := range messages { 
+			messageChan <- make_message(message)
+		}
+	// () : 해당 익명함수에 파라미터 전달
+	// ref. http://golang.site/go/article/21-Go-%EB%A3%A8%ED%8B%B4-goroutine
+	}() 
+	return messageChan
 }
 
-func send_message()
+func message_handler(client pb.BidirectionalClient, messageChan chan *pb.Message){
+	// 서버로부터 메시지 수신 & 서버로 메시지 전송
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	waitc := make(chan struct{})
+	stream, err := client.GetServerResponse(ctx)
+	if err != nil {
+		log.Fatalf("failed to creating a stream: %v", err)
+	}
+	//server to client
+	go func(){
+		 // 종료조건까지 무한 반복
+		for {
+			 // Receive from server
+			msg, err := stream.Recv()
+			if err == io.EOF {
+				// read done
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("failed to receive a message from the server")
+			}
+			fmt.Printf("[received from server] %s\n", msg)
+		}
+	}()
+	// send to server
+	for msg := range messageChan {
+		fmt.Printf("[send to server] %s \n", msg)
+		err := stream.Send(&pb.Message{Message: msg.GetMessage()}) // send messages to server
+		if err == io.EOF {
+			// send done
+			return
+		}
+		if err != nil{
+			log.Fatalf("failed to send a message to the server")
+		}
+	}
+	stream.CloseSend()
+	// waitc 채널이 닫히지 않는 이상 waitc 채널의 값을 수신하기 위해 계속대기 (blocking)
+	// 수신이 모두 완료될 경우 (err == io.EOF) 채널이 닫히면서 대기종료 
+	<-waitc
+}
 
 func main(){
-
+	conn,err := grpc.Dial(*addr,grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewBidirectionalClient(conn)
+	message_handler(client, generate_messages())
 }
--- bidirectional-streaming/server/server.go
+++ bidirectional-streaming/server/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -9,23 +11,47 @@ import (
 	"google.golang.org/grpc"
 )
 
+var (
+	// flag name, default value, usage message (--help)
+	port = flag.Int("port", 50051, "The server port")
+)
+
 type server struct{
 	pb.UnimplementedBidirectionalServer
 }
 
 func (s *server) GetServerResponse(stream pb.Bidirectional_GetServerResponseServer) error {
-	fmt.Printf("Server processing gRPC bidirectional streaming.")
+	fmt.Printf("Server processing gRPC bidirectional streaming.\n")
+	// 종료 조건까지 무한루프
 	for {
-		message, err := stream.Recv()
-		if err == nil {
+		// Receive messages from client
+		msg, err := stream.Recv() 
+		if err == io.EOF { 
+			// no more messages
+			return nil
+		  }
+		if err != nil {
+			log.Fatalf("failed to Recv: %v", err)
 			return err
 		}
-		fmt.Println(message)
+		log.Printf("[received from client] %s", msg)
+
+		// send messages to client
+		err = stream.Send(&pb.Message{Message: msg.GetMessage()})
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("failed to Send: %v", err)
+			return err
+		}
+		log.Printf("[send to client (echo)] %s", msg)
 	}
 }
 
 func main() {
-	lis, err := net.Listen("tcp", "localhost:50051")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
 		// 에러 , 로그 출력 후 종료
 		log.Fatalf("failed to listen: %v", err)
